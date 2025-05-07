eb888bc81f929a18220cd2778189d32596c6f8ab
tavassoliachi <tavassoliachi@gmail.com>
2025-05-08 00:11:04 +0400

    feat: update translations

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // or '0.0.0.0' → this exposes to your local network
-    port: 5173, // optional, default is 5173
+    host: true,
+    port: 5173,
   },
-  base: "/", // ensure relative base path
+  base: "/",
 });
